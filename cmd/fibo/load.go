3439e3f7dadd44efaa546a2524266081906bbca4
d030837 <f.seeger@sap.com>
2019-05-07 11:26:27 +0200

    added GET method

--- cmd/fibo/load.go
+++ cmd/fibo/load.go
@@ -2,40 +2,69 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"errors"
-	"strconv"
 	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
-func getParameterSize(r *http.Request) (uint64, error) {
-	value := r.URL.Query().Get("s")
-	if len(value) == 0 {
-		return 0, errors.New("Paramter 's' missing or has no value")
-	}
+func parseSize(parameter string) (uint64, error) {
 	dim := uint64(1)
-	switch value[len(value)-1:] {
+	switch parameter[len(parameter)-1:] {
 	case "k", "K":
 		dim = 1024
-		value = value[:len(value)-1]
+		parameter = parameter[:len(parameter)-1]
 	case "m", "M":
 		dim = 1024 * 1024
-		value = value[:len(value)-1]
+		parameter = parameter[:len(parameter)-1]
 	}
-	s, err := strconv.ParseUint(value, 10, 32)
+	size, err := strconv.ParseUint(parameter, 10, 32)
 	if err != nil {
 		return 0, err
 	}
-	return dim * s, nil
+	return dim * size, nil
+}
+
+func writeBody(size uint64, w http.ResponseWriter) error {
+	in, err := os.Open("/dev/urandom")
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	buf := make([]byte, 1024)
+	for size > 1024 {
+		size -= 1024
+		if _, err = w.Write(buf); err != nil {
+			return err
+		}
+	}
+	_, err = w.Write(buf[:size])
+	return err
 }
 
 func handleLoad(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		// TODO: implement
-		fmt.Fprintln(w, "not implemented yet")
+		value := r.URL.Query().Get("s")
+		if len(value) == 0 {
+			log.Println("Paramter 's' missing or has no value")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		size, err := parseSize(value)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.Header().Add("Content-Type", "application/binary")
+		if err := writeBody(size, w); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+
 	case "PUT":
 		// read body
 		if r.Body == nil {
@@ -51,7 +80,8 @@ func handleLoad(w http.ResponseWriter, r *http.Request) {
 			}
 			fmt.Fprintf(w, "Fetched %d size byte\n", size)
 		}
-	default:		
+
+	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
